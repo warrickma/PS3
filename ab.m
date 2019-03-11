3342cef4d3d6afd2a3e65e5dbf606c47271002ad
warrickma <48229049+warrickma@users.noreply.github.com>
2019-03-11 17:26:53 -0400

    Delete ab.m

--- ab.m
+++
@@ -1,8 +0,0 @@
-x=element1*S1;
-ans=x*a;
-
-% S1 (stoichiometry matrix)
-% element1: molecular formula matrix; row: intermediate species;
-% column:CHONP
-
-%ans: a vector showing elemental balance, negative means exiting urea cycle. row: CHONP
